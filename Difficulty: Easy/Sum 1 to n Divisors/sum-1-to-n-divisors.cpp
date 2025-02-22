fa8309f0996e0cc9a421b2f4534cb4b1a40888fb
charanya312 <162348601+charanya312@users.noreply.github.com>
2025-02-22 10:06:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum 1 to n Divisors/sum-1-to-n-divisors.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int sumOfDivisors(int n) {
+        long long sum=0;
+        for(int j=1;j<=n;j++){
+            sum+=j*(n/j);
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans = ob.sumOfDivisors(N);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
